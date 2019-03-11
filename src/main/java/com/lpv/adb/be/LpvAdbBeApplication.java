558375625a79ed34c28d25966e85ba53b0ddf1e9
selimssevgi <selimssevgi@gmail.com>
2019-03-11 22:29:49 +0200

    initialize project

---
+++ src/main/java/com/lpv/adb/be/LpvAdbBeApplication.java
@@ -0,0 +1,13 @@
+package com.lpv.adb.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpvAdbBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LpvAdbBeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/lpv/adb/be/LpvAdbBeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lpv.adb.be;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LpvAdbBeApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
